1b2726bd44e2bc753cbe0552da433da7265a9550
murilokapaz <murilodevkapaz@gmail.com>
2021-06-21 10:56:15 -0300

    useMemo and useRef

--- hooks/src/views/examples/UseMemo.jsx
+++ hooks/src/views/examples/UseMemo.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+
+function sum(x, y) {
+    const a = parseInt(x);
+    const b = parseInt(y);
+    const future = Date.now() + 2000;//simula um delay
+    while (Date.now() < future) { }
+    return a + b;
+}
+
 const UseMemo = (props) => {
+    const [n1, setN1] = useState(0);
+    const [n2, setN2] = useState(0);
+    const [n3, setN3] = useState(0);
+    // const [result, setResult] = useState(0);
+
+    // useEffect(()=>{
+    //     setResult(sum(n1,n2))
+    // },[n1, n2]);
+
+    //ou
+
+    const result = useMemo(_ => sum(n1, n2), [n1, n2]);
+
     return (
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
                 subtitle="Retorna um valor memoizado!"
             />
+            <div className="center">
+                <input
+                    type="number"
+                    className="input"
+                    value={n1}
+                    onChange={e => setN1(e.target.value)}
+                />
+                <input
+                    type="number"
+                    className="input"
+                    value={n2}
+                    onChange={e => setN2(e.target.value)}
+                />
+                <input
+                    type="number"
+                    className="input"
+                    value={n3}
+                    onChange={e => setN3(e.target.value)}
+                />
+                <span className="text">{result}</span>
+            </div>
         </div>
     )
 }
