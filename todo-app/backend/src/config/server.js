b228b57408d97107bd912bf7af3a8b6d211425ce
Murilo <murilokapaz@outlook.com>
2021-06-30 11:11:51 -0300

    backend made

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -1,13 +1,15 @@
-const port = 3003;
+const port = 3003
 
-const bodyParser = require('body-parser');
-const express = require('express');
+const express = require('express')
 const server = express();
+const allowCors = require('./cors');
 
-server.use(express.urlencoded({extended:true}));
-//extended suporta mais tipos de dados que o padrão
-server.use(express.json());
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(allowCors);
 
-server.listen(port, function(){
-    console.log(`BACKEND is running on port ${port}`);
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
