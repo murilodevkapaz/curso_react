fad8158b5d84e55aa239e11b584d6ec2c7fcc344
Murilo <murilokapaz@outlook.com>
2021-07-06 11:35:23 -0300

    main structure project created

---
+++ todo-app/frontend/src/main/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import  'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+
+export default props =>(
+    <div className="container">
+        <h1>Test</h1>
+    </div>
+)
--- todo-app/frontend/webpack.config.js
+++ todo-app/frontend/webpack.config.js
@@ -1,38 +1,38 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
-    output:{
+    output: {
         path: __dirname + '/public',
         filename: './app.js'
     },
-    devServer:{
+    devServer: {
         port: 8080,
         contentBase: './public',
-
     },
-    resolve:{
-        extensions: ['', '.js','.jsx'],
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
-    modules: {
+    module: {
         loaders: [{
             test: /.js[x]?$/,
-            loader: /node_modules/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        },{
-            test: /\css$/,
+        }, {
+            test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        },{
+        }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
