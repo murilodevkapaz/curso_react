c64d9f4792c8b44e2e48c70776af07759106f6e1
murilo.paz-ext@hexagon.com <murilo.paz-ext@hexagon.com>
2021-08-04 12:15:50 -0300

    components todo created

--- todo-app/frontend-sem-redux/src/todo/todo.jsx
+++ todo-app/frontend-sem-redux/src/todo/todo.jsx
@@ -1,15 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
-export default class Todo extends Component{
-    render(){
-        return(
+export default class Todo extends Component {
+
+    constructor(props){
+        super(props);
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleAdd() {
+        console.log('Add');
+    }
+
+    render() {
+        return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm/>
-                <TodoList/>
+                <TodoForm handleAdd={this.handleAdd} />
+                <TodoList />
             </div>
         )
     }
