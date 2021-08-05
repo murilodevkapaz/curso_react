e250517a07e9ddb7e54fca45deac95203a9c1ca1
murilo.paz-ext@hexagon.com <murilo.paz-ext@hexagon.com>
2021-08-05 10:11:27 -0300

    integration with mongodb started

--- todo-app/frontend-sem-redux/src/todo/todo.jsx
+++ todo-app/frontend-sem-redux/src/todo/todo.jsx
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
+
+const URL = 'http://localhost:3003/api/todos'
 export default class Todo extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = { description: 's', list: [] };
+
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.refresh();
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createAt`)
+            .then(resp => this.setState({
+                ...this.state,
+                description: '',
+                list: resp.data
+            }));
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value });
     }
 
     handleAdd() {
-        console.log('Add');
+        const description = this.state.description;
+
+        axios.post(URL, { description })
+            .then(resp => this.refresh());
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh());
+    }
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh());
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh());
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd} />
-                <TodoList />
+                <TodoForm
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                    description={this.state.description}
+                />
+                <TodoList
+                    list={this.state.list}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                    handleRemove={this.handleRemove}
+                />
             </div>
         )
     }
--- todo-app/frontend-sem-redux/src/todo/todoList.jsx
+++ todo-app/frontend-sem-redux/src/todo/todoList.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import IconButton from '../template/iconButton';
 
-export default props =>(
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || [];
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markAsDone' : ''}>{todo.description}</td>
+                <td>
+                    <IconButton
+                        style='success'
+                        hide={todo.done}
+                        icon='check'
+                        onClick={_ => props.handleMarkAsDone(todo)}
+                    ></IconButton>
+                    <IconButton
+                        style='warning'
+                        icon='undo'
+                        hide={!todo.done}
+                        onClick={_ => props.handleMarkAsPending(todo)}
+                    ></IconButton>
+                    <IconButton
+                        style='danger'
+                        icon='trash-o'
+                        hide={!todo.done}
+                        onClick={_ => props.handleRemove(todo)}
+                    ></IconButton>
+                </td>
+            </tr>
+        )
+        )
+    }
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
